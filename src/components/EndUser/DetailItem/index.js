41a4a477949994f015e9c8f8699256b9940cabed
KISE NGUYEN <loitan.nguyennguyen@gmail.com>
2022-03-16 17:06:40 +0700

    code detail

--- src/components/EndUser/DetailItem/index.js
+++ src/components/EndUser/DetailItem/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function index() {
     return (
-        <div>index</div>
+        <div className='container'>
+            <div className='detail'>
+                <div className='detail-img'>
+                    <img src="https://macstores.vn/wp-content/uploads/2019/07/macbook-pro-13inch-2019-mv962-6.jpg" alt="mac" />
+                </div>
+                <div className='detail-infor'></div>
+                <div className='detail-support'></div>
+            </div>
+        </div>
     )
 }
